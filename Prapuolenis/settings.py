8f1405571f13fdd501418c1563a5bb1b77ef00ea
Flatbyte <jonprapuolenis@gmail.com>
2015-10-19 16:57:53 -0500

    Day 2, I was finally broken when I needed to get into figuring out where the admin stuff all went wrong.

--- Prapuolenis/settings.py
+++ Prapuolenis/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'FBAPI',
+    'Prapuolenis.FBAPI',
+    'Prapuolenis.HomePg',
 )
 
 MIDDLEWARE_CLASSES = (
