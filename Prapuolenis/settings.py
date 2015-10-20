2f199a90074452cbb61f517a1eac5c265e033527
Flatbyte <jonprapuolenis@gmail.com>
2015-10-20 14:12:08 -0500

    To get the red names to be unconflicted.

--- Prapuolenis/settings.py
+++ Prapuolenis/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Prapuolenis.FBAPI',
-    'Prapuolenis.HomePg',
+
 )
 
 MIDDLEWARE_CLASSES = (
--- Prapuolenis/urls.py
+++ Prapuolenis/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+admin.autodiscover()
 
-from HomePg import views
+from  django.views.generic import TemplateView
 
 urlpatterns = (
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', views.index, name='index'),
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
 )
